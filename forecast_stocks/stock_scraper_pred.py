e22b4aa47fe9494f3dd5e097965e0d866b1ca4db
Bryan Bruno <44584575+brunster@users.noreply.github.com>
2022-04-02 22:22:20 -0700

    Add files via upload

--- forecast_stocks/stock_scraper_pred.py
+++ forecast_stocks/stock_scraper_pred.py
@@ -37,7 +37,7 @@ for t in range(len(tickers)):
     sec_tickers.append(sec_tick)
 
 choices = dict(zip(sec, tickers))
-ticker_select = st.selectbox("Select ", options = choices, format_func = lambda x: list(choices.keys())[x])
+ticker_select = st.selectbox("Select ", options = sec_tickers, format_func = lambda x: tickers[x])
 
 load_state = st.text("Downloading Stock Data...")
 data = load_data(ticker_select)
